f091321084c1cac9264bccce9eb4ba671ce19202
Rowan Polster <rowandownriver94@gmail.com>
2021-09-05 16:52:22 -0500

    completed

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,56 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Value of y must be greater than 0.");
+            }
+
+            double score = x / y;
+
+            return score;
+
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int score = 0;
+
+            string result = fileName.Substring(fileName.Length - 3);
+
+            if (result == ".cs")
+            {
+                score += 1;
+
+                return score;
+
+            } else if (result == "" || result == null)
+            {
+
+                throw new ArgumentNullException("Please submit a file for grading!");
+
+            }
+
+            return score;
+
         }
-        */
+
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            try
+            {
+                Console.WriteLine(Divide(80, 100));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,6 +65,24 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+
+            foreach (var student in students)
+            {
+                try
+                {
+                    string fileName = student.Value;
+
+                    Console.WriteLine($"{student.Key}: {CheckFileExtension(fileName)}");
+                    
+
+                }
+                catch
+                {
+                    Console.WriteLine($"{student.Key}: File is not of type .cs. Score is 0.");
+                }
+
+
+            }
 
         }
     }
